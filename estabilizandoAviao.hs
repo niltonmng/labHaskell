47b62eef6155128fe43333944902f0e7c99201f9
NILTON MANUEL NOGUEIRA GINANI 115211281 <niltonmng@lcc2-15.lcc.ufcg.edu.br>
2017-06-23 15:54:00 -0300

    aviao

--- estabilizandoAviao.hs
+++ estabilizandoAviao.hs
@@ -1,10 +1,28 @@
-calculaDiferenca :: Int -> Int -> String
-calculaDiferenca  |
+aviao :: (Int, Int) -> IO()
+aviao(ideal, dif) = do
+  if(dif) == 0
+    then putStrLn "OK"
+  else do
+    h3 <- getLine
+    let alturaIdeal3 = read h3
+    let moduloNum2 = abs(alturaIdeal3 - ideal)
+    if(dif >= moduloNum2)
+      then do
+        putStrLn "ADEQUADO"
+        aviao(ideal, moduloNum2)
+    else do
+        putStrLn "PERIGO"
+        aviao(ideal, moduloNum2)
 
 
 
-main :: IO ()
+
+
 main = do
-  x <- getLine
-	let primeira = read x
-  print("ADEQUADO")
+  h <- getLine
+  let alturaIdeal = read h
+  h1 <- getLine
+  let alturaAtual = read h1
+  let moduloNum = abs(alturaIdeal - alturaAtual)
+
+  aviao (alturaIdeal, moduloNum)
